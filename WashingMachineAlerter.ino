cfefaf3ad546dd41725f7fdd54544dcbe1551461
Andrew <lyfeshomebrew@gmail.com>
2019-06-06 20:26:52 -0700

    Update WashingMachineAlerter.ino
    Improved to not require button, alarm turns off when led does, improved audibility of tones

--- WashingMachineAlerter.ino
+++ WashingMachineAlerter.ino
@@ -4,21 +4,23 @@
 
 #define speakerPinA 3
 #define speakerPinB 4
-#define resetAlarmButton 5
-#define toneA 7
-#define toneB 9
-#define timePerToneInMillis 750     //(3/4ths second)
+#define toneA 1
+#define toneB 4
+#define timePerToneInMillis 750
 #define alarmThreshholdBrightness 200
 
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_4X);
 
 volatile unsigned long speakerTimer;
 volatile unsigned long TimeSinceLastVoltageReversal;
+volatile unsigned long TimeSinceLastStatusCheck;
 volatile boolean       toneState;
 volatile boolean       alarmReset = false;
 volatile boolean       speakerPinState;
 uint16_t clear, red, green, blue;
-  
+
+
 void setup() {
   Serial.begin(115200);
   if (tcs.begin()) {
@@ -27,13 +29,11 @@ void setup() {
     Serial.println("No sensor found");
     while (1); //halt
   }
-
   pinMode(speakerPinA, OUTPUT);
   pinMode(speakerPinB, OUTPUT);
-
   tcs.setInterrupt(true);
-
 }
+
 
 void loop() {
   updateBrightness();
@@ -45,34 +45,39 @@ void loop() {
   }
 }
 
+
 void updateBrightness() {
-  delay(60);  // takes 50ms to read
   tcs.getRawData(&red, &green, &blue, &clear);
   Serial.println("Green level: " + String(green));
 }
 
-void alarmLoop() {
-  while (!alarmReset) {
-    if (digitalRead(resetAlarmButton)) {
-      Serial.println(F("Alarm Button Pushed"));
-      alarmReset = true;
-      digitalWrite(speakerPinA, LOW);
-      digitalWrite(speakerPinB, LOW);
-      return;
-    }
 
-    if (speakerTimer + timePerToneInMillis < millis()) {
+void alarmLoop() {
+  TimeSinceLastStatusCheck = millis();
+  while (!alarmReset){
+    
+    if (speakerTimer + timePerToneInMillis < millis()){
       speakerTimer = millis();
       toneState = !toneState;
+      updateBrightness();
+      if (green < alarmThreshholdBrightness){
+        digitalWrite(speakerPinA, LOW);
+        digitalWrite(speakerPinB, LOW);
+        alarmReset = true;
+        Serial.println(F("Alarm Reset"));
+        return;
+      }
     }
-
-    //lazy copy-paste
-    if (toneState) {
-      if (TimeSinceLastVoltageReversal + toneA < millis()) {
+ 
+    if (toneState){
+      
+      if (TimeSinceLastVoltageReversal + toneA < millis()){
         swapPolarity();
         TimeSinceLastVoltageReversal = millis();
       }
+      
     } else {
+      
       if (TimeSinceLastVoltageReversal + toneB < millis()) {
         swapPolarity();
         TimeSinceLastVoltageReversal = millis();
@@ -81,7 +86,7 @@ void alarmLoop() {
   }
 }
 
-void swapPolarity(){
+void swapPolarity() {
   //Serial.println(F("Swapped polarity"));
   speakerPinState = !speakerPinState;
   digitalWrite (speakerPinA, speakerPinState);
